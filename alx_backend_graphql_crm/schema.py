d0f022b7616090cb03cf27718662b6207cccd9f8
blip <ryanniiakweibrown1@gmail.com>
2025-08-17 23:42:13 +0000

    Refactor schema to implement minimal Query class for Task 0 compliance

--- alx_backend_graphql_crm/schema.py
+++ alx_backend_graphql_crm/schema.py
@@ -1,11 +1,11 @@
+
+
+
+
+# Minimal schema for Task 0 compliance
 import graphene
-from crm.schema import Mutation as CRMMutation
-from crm.schema import Query as CRMQuery
 
-class Query(CRMQuery, graphene.ObjectType):
-    pass
+class Query(graphene.ObjectType):
+    hello = graphene.String(default_value="Hello, GraphQL!")
 
-class Mutation(CRMMutation, graphene.ObjectType):
-    pass
-
-schema = graphene.Schema(query=Query, mutation=Mutation)
+schema = graphene.Schema(query=Query)
